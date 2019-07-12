a68e18d1ac9a281d0625318776ba28c4fe1d4575
Ron Rodriguez <zarodrig@ucalgary.ca>
2019-07-12 16:23:09 -0600

    Add a TestFile.java to demonstrate git push

---
+++ ron/TestFile.java
@@ -0,0 +1,25 @@
+//////////////////////////////////////
+//
+//  File: testfile.java
+//  Author: Ron Rodriguez
+//  Course: CPSC 233 Summer 2019
+//  Creation Date: July 12, 2019
+//  Version: 0.01
+//  Revision Date: July 12, 2019
+//
+//  Description: test java class  
+//
+//////////////////////////////////////
+
+
+public class TestFile {
+
+	public static void main ( String[] args){
+
+	System.out.println("This is a test");
+	
+	return;
+
+	}
+
+}
