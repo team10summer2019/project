17c19b6c707c90ad9fa79779a73e94c82e88bdf3
zarodrig <zarodrig@zone33-wd.cs.ucalgary.ca>
2019-07-15 17:14:11 -0600

    update file with new output

--- folder/src/test.java
+++ folder/src/test.java
@@ -2,10 +2,11 @@
 
 public class test {
 
-	
-	
 	public static void main (String[] args) {
+	int var = 3	;
 	System.out.println("This is a test");
+	System.out.print("One Two Three " + var);
+	
 	}
 	
 }
