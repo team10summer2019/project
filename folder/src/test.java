39dbb74c7f220396f2ccacad931f0b9401bf263d
steven <steven@10.0.0.193>
2019-07-16 09:42:59 -0600

    test

--- folder/src/test.java
+++ folder/src/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main (String[] args) {
 	int var = 3	;
-	System.out.println("This is a test");
+	System.out.println("This is a test from me");
 	System.out.print("One Two Three " + var);
 	
 	}
