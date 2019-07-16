aad16dd491edfe7cce8f184b7635cdf2fde7a9b3
steven <steven@10.0.0.193>
2019-07-16 09:48:20 -0600

    testing

--- TestFile.java
+++ TestFile.java
@@ -17,7 +17,7 @@ public class TestFile {
 	public static void main ( String[] args){
 
 	System.out.println("This is a test");
-	System.out.println("This is a test");
+	System.out.println("This is a test from me");
 	System.out.println("TestingFiona");
 	System.out.println("This is a test kate");
 	
