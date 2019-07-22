c77b4ca381b533b42ee116b92e0af0857d34dbff
steven <steven@10.0.0.193>
2019-07-22 14:53:40 -0600

    New changes

---
+++ riverPuzzle/TestStevenRiver.java
@@ -0,0 +1,12 @@
+
+public class TestStevenRiver {
+	public static void main(String[] args) {
+		StevenRiver test = new StevenRiver();
+		test.problem();
+		test.start();
+		test.checkWin();
+
+		}
+	}
+		
+	  
