659eca0cb29268043fe023eef80a66506d9b9061
steven <steven@10.0.0.193>
2019-07-21 23:00:09 -0600

    I still need edits to make it more clean but the puzzle is done

---
+++ River Puzzle/TestStevenRiver.java
@@ -0,0 +1,11 @@
+
+public class TestStevenRiver {
+	public static void main(String[] args) {
+		StevenRiver test = new StevenRiver();
+		test.problem();
+		test.start();
+		test.checkWin();
+		}
+	}
+		
+	
