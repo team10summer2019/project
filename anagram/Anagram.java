adc334a81a38a13a42e1201fb6f13d90cba0cee2
whkate <52931661+whkate@users.noreply.github.com>
2019-07-26 18:10:23 -0600

    Anagram revised

--- anagram/Anagram.java
+++ anagram/Anagram.java
@@ -1,29 +1,90 @@
-package anagram;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 
-// save to vase
 public class Anagram {
-	public static ArrayList<String> bookList = new ArrayList<>(
-			Arrays.asList("Storm of sword", "A song of ice and fire", "Velveteen Rabbit ", "Ender's game"));
-			// first letter of each book - SAVE
-			
-	public static ArrayList<String> switchBookPosition() {
-		System.out.println(bookList);
-		System.out.println("Which book do you want to move?");
-		Scanner book1 = new Scanner(System.in);
-		int bookone = book1.nextInt(); 
-		System.out.println("switch book with?");
-		Scanner book2 = new Scanner(System.in);
-		int booktwo = book2.nextInt();
-		Collections.swap(bookList, bookone, booktwo);
-		System.out.println(bookList);
-		return bookList;
+	private ArrayList<Character> word = new ArrayList<>();
+	
+	public String AnagramQuestion(){
+		ArrayList<String> question = new ArrayList<>(
+				//question = list of anagram questions
+				Arrays.asList("SAVE", "CAT", "LISTEN")); //answer: vase, act, silent
+		//randomly get one of the question from question list
+		Random rand = new Random();
+		int num = rand.nextInt(3);
+		return question.get(num); //return one of the three question
 	}
 	
-	public static void main(String[] args) {
-		switchBookPosition();
+	public ArrayList<Character> getQuestion(){
+		String que = AnagramQuestion();
+		int stringLength = que.length();
+		int testlength = 0;
+		//get letter of question and add each letter to list(word)
+		while (testlength != stringLength) {
+			char letter = que.charAt(testlength);
+			word.add(letter);
+			testlength ++;
+		}
+		//System.out.println(word);
+		return word; //return word to be rearrange
 	}
+	
+	public boolean checkAnswer() {
+		if (word.size() == 3 ) {
+			if (word.get(0)== 'A' && word.get(1) == 'C' && word.get(2)== 'T') {
+				System.out.println("Win");
+				return true;
+			}
+			else {
+				System.out.println("try again!" + word);
+				getInput();
+				checkAnswer();
+			}
+		} 
+		else if (word.size() == 4 ) {
+			if (word.get(0)== 'V' && word.get(1)== 'A' && word.get(2)== 'S' && word.get(3)== 'E') {
+				System.out.println("Win");
+				return true;
+			} else {
+				System.out.println("try again!" + word);
+				getInput();
+				checkAnswer();
+			}
+		} 
+		else if (word.size() == 6 ) {
+			if (word.get(0)== 'S' && word.get(1)== 'I' && word.get(2)== 'L' && word.get(3)== 'E' && word.get(4)== 'N'&& word.get(5)== 'T') {
+				System.out.println("Win");
+				return true;
+			} else {
+				System.out.println("try again!" + word);
+				getInput();
+				checkAnswer();
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
+	public ArrayList<Character> getInput() {
+		Scanner input = new Scanner(System.in);
+		int answer = input.nextInt();//enter 2 numbers
+		int answer1 = answer/10;
+		int answer2 = answer%10;
+		Collections.swap(word, answer1-1, answer2-1);
+		return word;
+	}
+	
+	public void askQuestion() {
+		
+		System.out.println("rearranging the letters into another word");
+		System.out.println("Choose two letter to switch place");
+		System.out.println("enter two number (eg. enter 12 if rearrange letter 1 and 2)");
+		System.out.println(word);
+		getInput();
+	}
+
 }
