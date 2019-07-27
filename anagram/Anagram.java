9ac2dc7f58a57225c1a12a6dc1a1843091bffe7a
whkate <52931661+whkate@users.noreply.github.com>
2019-07-26 18:12:21 -0600

    anagram revised

--- anagram/Anagram.java
+++ anagram/Anagram.java
@@ -1,3 +1,4 @@
+package anagram;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
---
+++ anagram/TestAnagram.java
@@ -0,0 +1,10 @@
+package anagram;
+public class TestAnagram {
+	public static void main(String[] args) {
+		Anagram test = new Anagram();
+		test.getQuestion();
+		test.askQuestion();
+		test.checkAnswer();
+		
+	}
+}
