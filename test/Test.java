c78deafb61eb31162d418a7453abd079b602d4e0
zarodrig <zarodrig@ucalgary.ca>
2019-07-31 18:26:28 -0600

    Change function to rons way

--- test/Test.java
+++ test/Test.java
@@ -18,6 +18,8 @@ public Test(int awesome) {
 
 }
 
+
+
 //////////////////   ACCESSORS /////////////////////////////
 
 
@@ -28,11 +30,28 @@ public Test(int awesome) {
 	public void setVal1(String aString) {
 		this.var1 = aString;
 	}
+
+	// COOMMENT   This function will be replaced send me email
+	// there  is a better way
+	/*
 	public void setVal2(String aString2) {
 		this.var2 = aString2;
 	}
+	*/
 
+//////// THis is rons work 
 
+	/// COMMENT FROM RON:
+	/// This function sucks I'm going to overload it and delete it  
+	///  I have a better function waith 30 mins work on another function
+	public void SetVal2(String aString){
+		
+	 	this.var2 = aString;
+
+	}
+	
+	
+///////////   END OF RON's work
 
 /////////////////// OTHER  ////////////////////////////////
 
