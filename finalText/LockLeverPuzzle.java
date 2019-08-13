7c03db439ad355d236cbd32923cdbc42d2241233
haine.kim1 <haine.kim1@10.0.0.128>
2019-08-13 01:08:19 -0600

    most up to date text version. picking up cabbage, goat, wolf will not remove item from room.

---
+++ finalText/LockLeverPuzzle.java
@@ -0,0 +1,65 @@
+package finalText;
+
+
+/**
+ * @author Fiona
+ *
+ */
+public class LockLeverPuzzle extends GenericComboLock {
+	/*
+	 * CLASS
+	 * LOCK LEVER PUZZLE
+	 * lock lever puzzle has the same traits as GenericComboLock
+	 * an additional lever key is set to act as a key
+	 * puzzle will unlock both combination and lever are put in the right position
+	 */
+	private boolean lever_canUse;
+	private String lever_position;
+	private boolean correctLeverPosition;
+	
+	//CONSTRUCTORS
+	public LockLeverPuzzle(int comboNum1_Key, int comboNum2_Key, int comboNum3_Key) {
+		super(comboNum1_Key,comboNum2_Key,comboNum3_Key);
+	}
+	
+	//METHODS
+	public void information() {
+		//if user had click on it, this initializes first thing
+		System.out.print("This contraption..this looks like a combination lock. Theres three gears, each with a shape drawn right underneath it.\nThere's a hole on the side of the contraption.");
+		super.checkCombo();
+	}
+	
+	public boolean checkLever() { //check if obj_Lever_Key exists
+		boolean leverExists = false;
+		
+		LockLever lever = new LockLever();
+		if  (lever.combinedLever() == true) {
+			lever_canUse = true;
+		}
+		
+		return leverExists; 
+	}
+	
+	public void shiftLever() {//forwards,backwards,neutral
+		if (lever_canUse == true) {
+			if (lever_position == "neutral") { //Unnecessary, only used for checks(neutral and backwards)
+				System.out.println("in neutral");
+			}
+			if (lever_position == "toward") {
+				System.out.println("towards you");
+			}
+			if (lever_position == "away") {
+				System.out.println("away from you");
+				correctLeverPosition = true;
+			}
+		} else {
+			System.out.println("The hole on the side of the contraption must do something. Maybe something can help activate it.");
+		}
+	}
+	
+	public void solvePuzzle() {
+		if ((super.checkCombo() == true) && (correctLeverPosition == true)) {
+			System.out.println("The door opens to the next room");
+		}
+	}
+}
---
+++ finalText/TestHaineRiddle.java
@@ -0,0 +1,75 @@
+package finalText;
+
+public class TestHaineRiddle { 
+	 
+	public boolean riddleOne(){
+		///MAKE A LOT OF RIDDLES 
+		CreateRiddle runner = new CreateRiddle();
+		CreateRiddle riddle1 = new CreateRiddle("HUMAN" ,"What is the creature that walks on four legs in the morning,\ntwo legs at noon, and three in the evening?\n");
+		CreateRiddle riddle2 = new CreateRiddle("PENNY", "What has a head, a tail, is brown, and has no legs?\n");
+		CreateRiddle riddle3 = new CreateRiddle("BLACKBOARD","I get whiter the dirtier I get. What am I?\n");
+		CreateRiddle riddle4 = new CreateRiddle("ICE","Made from water, but dies in water. What am I?\n");
+		runner.addRiddles(riddle1);
+		runner.addRiddles(riddle2);
+		runner.addRiddles(riddle3);
+		runner.addRiddles(riddle4);
+		
+		CreateRiddle theRiddle = runner.chooseRiddle();
+		runner.addAnswer(theRiddle);
+		runner.sayRiddle(theRiddle);
+		 
+		runner.instructions();
+		runner.getInput();
+		
+		boolean solved = runner.checkAnswer();
+		return solved;
+		
+	}
+	
+	public boolean riddleTwo(){
+		///MAKE A LOT OF RIDDLES
+		CreateRiddle runner = new CreateRiddle();
+		CreateRiddle riddle5 = new CreateRiddle("OWL" ,"I always ask but never answer. What do you think I am?\n");
+		CreateRiddle riddle6 = new CreateRiddle("EGG", "You can only use me after you break me. What am I?\n");
+		CreateRiddle riddle7 = new CreateRiddle("BREATH","What can you hold without using your hands or arms?\n");
+		runner.addRiddles(riddle5);
+		runner.addRiddles(riddle6);
+		runner.addRiddles(riddle7);
+		
+		CreateRiddle theRiddle = runner.chooseRiddle();
+		runner.addAnswer(theRiddle);
+		runner.sayRiddle(theRiddle);
+		 
+		runner.instructions();
+		runner.getInput();
+		
+		boolean solved = runner.checkAnswer();
+		return solved;
+		
+	}
+	
+	public boolean riddleThree(){
+		///MAKE A LOT OF RIDDLES
+		CreateRiddle runner = new CreateRiddle();
+		CreateRiddle riddle8 = new CreateRiddle("FLEAS" ,"When they are caught, they are thrown away. When they escape, you itch all day. What are they?\n");
+		CreateRiddle riddle9 = new CreateRiddle("G", "What always ends everything?\n");
+		CreateRiddle riddle10 = new CreateRiddle("KEYBOARD","I have keys, but no locks. I have a space, but no room.\nYou can enter but you can't go outside. What am I?\n");
+		CreateRiddle riddle11 = new CreateRiddle("TOWEL","What gets wet when drying?");
+		runner.addRiddles(riddle8);
+		runner.addRiddles(riddle9);
+		runner.addRiddles(riddle10);
+		runner.addRiddles(riddle11);
+		
+		CreateRiddle theRiddle = runner.chooseRiddle();
+		runner.addAnswer(theRiddle);
+		runner.sayRiddle(theRiddle);
+		 
+		runner.instructions();
+		runner.getInput();
+		
+		boolean solved = runner.checkAnswer();
+		return solved;
+		
+	}
+	
+}
